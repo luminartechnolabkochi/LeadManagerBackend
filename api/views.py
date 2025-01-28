4cc848f7b94e0011c7c3950e1c96d81c976dd662
sajay <sajaykannan10@gmail.com>
2025-01-28 08:54:37 +0530

    added serach api

--- api/views.py
+++ api/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render
 from rest_framework.generics import CreateAPIView,ListAPIView,RetrieveAPIView,UpdateAPIView,DestroyAPIView
 from api.serializers import LeadSerializer
 from api.models import Lead
+from django.db.models import Q
 
 class LeadListCreateView(CreateAPIView,ListAPIView):
 
@@ -15,6 +16,34 @@ class LeadListCreateView(CreateAPIView,ListAPIView):
 
     queryset=Lead.objects.all()
 
+    def get_queryset(self):
+        
+        #http://127.0.0.1:8000/api/leads/?search_text=bin
+
+        filter_text=self.request.query_params #{"search_text":"bin"}
+
+        qs=Lead.objects.all()
+
+        if filter_text:
+
+            search_value=filter_text.get("search_text")
+
+            qs=qs.filter(Q(name__contains=search_value)|Q(email__contains=search_value))
+
+        return qs
+
+        
+
+
+
+
+
+
+    
+
+
+
+    
 
 
 class LeadRetrieveUpdateDestroyView(RetrieveAPIView,UpdateAPIView,DestroyAPIView):
